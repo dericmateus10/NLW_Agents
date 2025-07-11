c54b4f1af3e02b692475e9239285137e1a866b51
Deric Mateus <dericmateus10@gmail.com>
2025-07-11 12:02:30 -0300

    Add Biome configuration, Docker setup, and update dependencies
    - Introduced biome.jsonc for Biome configuration.
    - Added docker-compose.yml for PostgreSQL service setup.
    - Created setup.sql for PostgreSQL extension.
    - Updated package.json with new devDependencies.
    - Refactored env.ts and server.ts for improved code structure.
    - Enhanced tsconfig.json with strictNullChecks option.

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,23 @@
 import { fastifyCors } from '@fastify/cors';
 import { fastify } from 'fastify';
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
-
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
+app.get('/health', () => {
+  return 'ok';
+});
 
-
-app.listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333}).then(() => {
-    console.log('Server is running! on http://localhost:3333');
-})
+app.listen({ port: env.PORT });
